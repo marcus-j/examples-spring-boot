3b683e21982b8431df9a60c513a9591d2f3919b6
marcus-j <18111840+marcus-j@users.noreply.github.com>
2019-03-03 08:47:33 +0100

    cleans up MyConfigurationTest

--- spring-boot-hello/src/test/java/de/marcusjanke/examples/spring/boot/hello/MyConfigurationTest.java
+++ spring-boot-hello/src/test/java/de/marcusjanke/examples/spring/boot/hello/MyConfigurationTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class MyConfigurationTest {
 	
 	@Autowired
-	public MyBean myBean;
+	private MyBean myBean;
 
 	@Test
 	public void testMyConfiguration() {
